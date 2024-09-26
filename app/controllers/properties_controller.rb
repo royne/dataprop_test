4e6eabb3dc5e2cd307bf00667d8d4539c503fdd5
royne <royne.chaves@gmail.com>
2024-09-26 12:59:34 -0500

    add crud

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,9 +1,53 @@
 class PropertiesController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create edit update]
+  before_action :set_property, only: %i[edit update]
+  before_action :authorize_user!, only: %i[edit update]
+
   def index
     @properties = Property.all
   end
 
   def show
     @property = Property.find(params[:id])
+  end
+
+  def new
+    @property = current_user.properties.new
+  end
+
+  def create
+    @property = current_user.properties.new(property_params)
+    if @property.save
+      redirect_to @property, notice: "Property created."
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @property.update(property_params)
+      redirect_to @property, notice: "Property updated."
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_property
+    @property = current_user.properties.find(params[:id])
+  end
+
+  def authorize_user!
+    unless @property.user == current_user
+      redirect_to properties_path, alert: "not authorized"
+    end
+  end
+
+  def property_params
+    params.require(:property).permit(:property_type, :price, :currency, :commune, :address, :area, :bedrooms, :bathrooms, :latitude, :longitude, :description, :photo)
   end
 end
